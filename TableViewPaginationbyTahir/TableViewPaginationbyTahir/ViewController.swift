705cbe0289a3dae2886fd5070eb2db573f315c22
tahirios <tahirsvoice@gmail.com>
2023-07-05 01:10:45 +0500

    Add New Project

---
+++ TableViewPaginationbyTahir/TableViewPaginationbyTahir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPaginationbyTahir
+//
+//  Created by Tahir Mahmood on 05/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
